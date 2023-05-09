0277d6c40d52685d575d55673d46c5cb2e140ef6
Burak Ali ILDIR <burakaliildir@gmail.com>
2023-05-09 10:58:30 +0300

    ...

--- src/app/_resolvers/meets.resolver.ts
+++ src/app/_resolvers/meets.resolver.ts
@@ -1,7 +1,6 @@
 import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular/router";
 import { MeetService } from "../_services/meet.service";
-import { GetMessagesResponse } from "../_responses/messages.response";
 import {GetMeetsResponse} from "../_responses/meets.response";
 
 export const getMeetsResolver: ResolveFn<GetMeetsResponse> =
